266ab6c383e8bb1afb45dfc130f044bf005e3346
Logan Hsia <>
2023-10-13 16:10:37 -0400

    Updated sidebar.php to listen for and echo searchbar input

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,18 +1,32 @@
 <!-- Blog Sidebar Widgets Column -->
 <div class="col-md-4">
+    <?php
+
+//    TODO: Quick Submission Check
+//    check if we have a search submission
+    if(isset($_POST['submit'])){
+
+//        echo if we do
+        $search = $_POST['search'];
+        echo $search;
+
+    }
+
+    ?>
 
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
+        <form action="" method ="post">
         <div class="input-group">
-            <input type="text" class="form-control">
+            <input name="search" type="text" class="form-control"> <!-- TODO PCP: PHP associate label to input -->
             <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
+                            <button name="submit" class="btn btn-default" type="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
                         </span>
-        </div>
-        <!-- /.input-group -->
+        </div><!-- /.input-group -->
+        </form><!-- /.search form -->
     </div>
 
     <!-- Blog Categories Well -->
