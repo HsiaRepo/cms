591113b20195f8b2b45e4476abc4bee76a1e3134
HsiaRepo <logan@hsia.us>
2023-10-19 14:01:49 -0400

    created edit_post.php with edit functionality for posts. added edit_post as a switch case in view_all_posts

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -11,6 +11,7 @@
         <th>Tags</th>
         <th>Comments</th>
         <th>Date</th>
+        <th>Edit</th>
         <th>Delete</th>
     </tr>
     </thead>
@@ -83,6 +84,7 @@
         echo "<td>{$post_tags}</td>";
         echo "<td>{$post_comment_count_title}</td>";
         echo "<td>{$post_date}</td>";
+        echo "<td><a href='posts.php?source=edit_post&post_id={$post_id}'>Edit</a></td>";
         echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
         echo "</tr>";
 
